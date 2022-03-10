cda8fd82610935c92e7591c13842a13cd8cb22de
Kate <flukatete@yandex.ru>
2022-03-10 22:57:15 +0300

    Added name generation

--- 04_MergetoolCommandline/repl.py
+++ 04_MergetoolCommandline/repl.py
@@ -2,18 +2,57 @@ import cmd
 import shlex
 import pynames
 
+FNAMEGEN = 'FullnameGenerator'
+NAMEGEN = 'NamesGenerator'
+LANG_PREFIX = 'language=pynames.LANGUAGE.'
+GENDER_PREFIX = 'gender=pynames.GENDER.'
+
 class commandline(cmd.Cmd):
     intro = 'Welcome to command line name generator'
 
-    lang = 'native'
+    lang = 'NATIVE'
+    iron_kingdoms_subclasses = [k[:-17] for k in pynames.generators.iron_kingdoms.__dict__ if k.endswith(FNAMEGEN)]
+    elven_subclasses = [k[:-14] for k in pynames.generators.elven.__dict__ if k.endswith(NAMEGEN)]
 
     def do_language(self, args):
         """Set language"""
         params = shlex.split(args)
-        self.lang = 'native'
+        self.lang = 'NATIVE'
         if params:
-            if params[0].lower() in pynames.relations.LANGUAGE.ALL:
-                self.lang = params[0].lower()
+            if params[0].lower() in pynames.LANGUAGE.ALL:
+                self.lang = params[0].upper()
+
+    def do_generate(self, args):
+        """Generate a name"""
+        params = shlex.split(args)
+        if params:
+            if params[0] in pynames.generators.__all__:
+                gen_class_name = 'pynames.generators.' + params[0] + '.'
+                if params[0] == 'iron_kingdoms':
+                    if len(params) > 1 and params[1] in self.iron_kingdoms_subclasses:
+                        gen_class_name += params[1]
+                    else:
+                        gen_class_name += self.iron_kingdoms_subclasses[0]
+                    gen_class_name += FNAMEGEN
+                elif params[0] == 'elven':
+                    if len(params) > 1 and params[1] in self.elven_subclasses:
+                        gen_class_name += params[1]
+                    else:
+                        gen_class_name += self.elven_subclasses[0]
+                    gen_class_name += NAMEGEN
+                else:
+                    gen_class_name += [k for k in eval(gen_class_name + '__dict__') if k.endswith(NAMEGEN)][0]
+
+                gender = ''
+                if len(params) > 1 and params[-1].lower() in pynames.GENDER.ALL:
+                    gender = GENDER_PREFIX + params[-1].upper()
+
+                gen_params = (gender + ', ') if gender else ''
+                gen_params += (LANG_PREFIX + self.lang) if eval(LANG_PREFIX[9:] + self.lang) in eval(gen_class_name + '().languages') else ''
+
+                result = eval(gen_class_name + '().get_name_simple' '(' + gen_params + ')')
+
+                print(result)
 
     def do_exit(self, args):
         return True
