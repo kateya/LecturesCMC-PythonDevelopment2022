d5120399ad7b5e3d13d01791819a4948f6e2ad55
Kate <flukatete@yandex.ru>
2022-03-14 21:37:33 +0300

    Added finding similar functions

--- 05_DiffPatchAST/dubfinder.py
+++ 05_DiffPatchAST/dubfinder.py
@@ -1,6 +1,9 @@
 from argparse import ArgumentParser
 from importlib import import_module
+from textwrap import dedent
+from difflib import SequenceMatcher
 import inspect
+import ast
 
 parser = ArgumentParser()
 parser.add_argument("module", type=str, nargs='+')
@@ -21,3 +24,39 @@ functions = []
 for module in args.module:
     mod = import_module(module)
     functions += get_functions(mod, module)
+
+prep_functions = {}
+
+for func in functions:
+    src = inspect.getsource(func[1])
+    if src.startswith(" "):
+        src = dedent(src)
+    tree = ast.parse(src)
+    for node in ast.walk(tree):
+        if hasattr(node, 'name'):
+            node.name = '_'
+        if hasattr(node, 'id'):
+            node.id = '_'
+        if hasattr(node, 'arg'):
+            node.arg = '_'
+        if hasattr(node, 'attr'):
+            node.attr = '_'
+    prep = ast.unparse(tree)
+    prep_functions[func[0]] = prep
+
+result = {}
+
+for f, p in prep_functions.items():
+    max = 0.0
+    fname = ''
+    for fm, pm in prep_functions.items():
+        if fm != f:
+            r = SequenceMatcher(None, p, pm).ratio()
+            if r > max:
+                max = r
+                fname = fm
+    if max > 0.95:
+        if result.get(fname) and result[fname] == f:
+            continue
+        result[f] = fname
+        print(*sorted([f, fname]))
